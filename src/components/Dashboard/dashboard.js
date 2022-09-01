514533dcb92ecc679283dbce5ab2e33bebece4b0
OgechukwuCSC205 <yomacorp54@gmail.com>
2022-09-01 13:27:55 +0100

    header and sidebar implemented

--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,26 +1,17 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import {Link} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
+import Header from "../base/Header/Header";
+import Sidebar from "../base/Sidebar/Sidebar";
 // import Form from "react-bootstrap/Form";
 import "./dashboard.css";
 
 const Dashboard = () => {
   return (
     <section>
-      <ul className="list">
-          <li className="links">
-            <Link to="/dashboard">Home</Link>
-          </li>
-          <li className="links">
-            <Link to="/tasks">Tasks</Link>
-          </li>
-          <li className="links">
-            <Link to="/settings">Settings</Link>
-          </li>
-          <li className="links further-down">
-            <Link to="/login">Logout</Link>
-          </li>
-        </ul>
+      <Header />
+      <Sidebar   />
+      <Outlet>
+      </Outlet>
     </section>
   );
 };
---
+++ src/components/base/Header/Header.js
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="topnav" id="myTopnav">
+        <div className="fa-div">
+          <p className="fa-p">
+            Samantha Okonkwo
+            <i className="fa fa-user iconic" aria-hidden="true"></i>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
